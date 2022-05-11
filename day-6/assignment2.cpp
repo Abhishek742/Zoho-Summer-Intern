2a0cfbc334e3ac360e9c2cc2701a0e1993a2779a
Abhishek742 <abhishekdurairaj2001@gmail.com>
2022-05-11 14:43:05 +0530

    day-6 t-2

---
+++ day-6/assignment2.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // try{
+    //     try{
+    //         throw out_of_range("out of range Error\n");
+    //     }catch(runtime_error &e){
+    //         cout<<e.what();
+    //         throw ;
+    //     }
+    // }catch(const char* s){
+    //     cout<<s<<"sksj";
+    // }catch(...){
+    //     cout<<""
+    // }
+    
+    //outer try block
+    try{
+        //inner try block
+        try{
+            cout<<"Inside inner try block 1 (throws runtime error)\n";
+            throw runtime_error("Runtime Error");
+        }catch(runtime_error& e){
+            cout<<e.what()<<" caught at inner catch block 1\n";
+        }
+        try{
+            cout<<"\nInside inner try block 2 (throws out of range error)\n";
+            throw out_of_range("Out of range");
+        }catch(out_of_range& e){
+            cout<<e.what()<<" caught at inner catch block 2\n";
+            throw;
+        }
+    }catch(runtime_error& e){
+            cout<<e.what()<<" caught at outer catch block 1\n";
+    }catch(...){
+        cout<<"No such notable exception found";
+    }
+    
+    return 0;
+}
